b60c4488689916f58a2671c8fd693fc516fa3a67
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-02-19 15:35:14 +0100

    terminal theming

--- etc/nixos/fonts.nix
+++ etc/nixos/fonts.nix
@@ -2,11 +2,19 @@
 
 {
   fonts.fontconfig.enable = true;
+
   fonts.fonts = with pkgs; [
     font-awesome
     material-icons
     powerline-fonts
     powerline-symbols
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-sans-static
+    noto-fonts-cjk-serif
+    noto-fonts-cjk-serif-static
+    noto-fonts-color-emoji
   ]
-  ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+  ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts)
+  ;
 }
