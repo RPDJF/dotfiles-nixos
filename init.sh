5b33642d791b7ca8d18063ce32a79e11a6952b88
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-02-20 19:12:15 +0100

    Refactor Hyprland configuration: add monitor profiles, update profile symlink management, and enhance init script for profile creation

--- init.sh
+++ init.sh
@@ -11,11 +11,20 @@ salt=${salt//$'\n'/}
 
 hashed_id=$(printf "%s%s" "$salt" "$machine_id" | sha256sum | awk '{print $1}')
 
-profile_dir="/etc/nixos/profiles/$hashed_id"
-sudo mkdir -p "$profile_dir"
+nix_profile_dir="/etc/nixos/profiles/$hashed_id"
+hypr_profile_dir="$HOME/.config/hypr/hyprland.profiles.d/$hashed_id"
+sudo mkdir -p "$nix_profile_dir"
+mkdir -p "$hypr_profile_dir"
 
 read -rp "Profile name for $hashed_id : " profile_name
 sudo rm -f "/etc/nixos/profiles/$profile_name"
 sudo ln -s "$hashed_id" "/etc/nixos/profiles/$profile_name"
+rm -f "$HOME/.config/hypr/hyprland.profiles.d/$profile_name"
+ln -s "$hashed_id" "$HOME/.config/hypr/hyprland.profiles.d/$profile_name"
 
-echo "✅  Profile '$profile_name' → $profile_dir created."
+# change current profile symlink to point to the new profile
+rm -f "$HOME/.config/hypr/hyprland.profiles.d/current"
+ln -s "$profile_name" "$HOME/.config/hypr/hyprland.profiles.d/current"
+
+echo "✅  Nix Profile '$profile_name' → $nix_profile_dir created."
+echo "✅  Hyprland Profile '$profile_name' → $hypr_profile_dir created."
