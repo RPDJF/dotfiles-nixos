6b07c1609ade5a5164aa32215f230e409176a4cc
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-02-21 19:25:25 +0100

    environment variables

--- etc/nixos/environment.nix
+++ etc/nixos/environment.nix
@@ -2,6 +2,7 @@
 
 {
   environment.variables.SSH_AUTH_SOCK = "/run/user/1000/gcr/ssh";
+  environment.variables.LANG = "fr_CH.UTF-8";
   environment.shellAliases = {
     confedit = "code $HOME/dotfiles-nixos";
     clear = "clear && printf \"\\e[3J\"";
