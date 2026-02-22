7d053e96a80d1701d3370927fa81f31c2e2c614d
RPDJF <86334233+RPDJF@users.noreply.github.com>
2026-02-22 23:11:33 +0100

    gaming env

--- etc/nixos/profiles/344041db2731d14fd6907868c7aa895362106f32e63f1eb0732fcda79965dc01/profile.nix
+++ etc/nixos/profiles/344041db2731d14fd6907868c7aa895362106f32e63f1eb0732fcda79965dc01/profile.nix
@@ -6,12 +6,19 @@
       ./network.nix
     ];
 
-    environment.variables = {
+    environment.variables = let
+      tasksetArgs = "-a -c 0-15";
+      gamescopeArgs = "-W 3840 -H 2160 -w 3840 -h 2160 --force-wayland --hdr-enabled --force-grab-cursor --adaptive-sync --rt --steam -f -r 240 --backend wayland";
+    in {
       LIBVA_DRIVER_NAME = "nvidia";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       GBM_BACKEND = "nvidia-drm";
-      PROTON_ENABLE_NVAPI="1";
-      PROTON_HIDE_NVIDIA_GPU="0";
-      PROTON_ENABLE_WAYLAND="1";
+      PROTON_ENABLE_NVAPI = "1";
+      PROTON_HIDE_NVIDIA_GPU = "0";
+      PROTON_ENABLE_WAYLAND = "1";
+
+      TASKSET_ARGS = tasksetArgs;
+      GAMESCOPE_ARGS = gamescopeArgs;
+      STEAM_ARGS = "taskset ${tasksetArgs} gamescope ${gamescopeArgs} -- %command%";
     };
 }
